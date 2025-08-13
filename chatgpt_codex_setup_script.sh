b6b77f3aea3698000c2a75e22b890ce8d066270c
Jesper Torp Kristensen <jespertk@gmail.com>
2025-08-13 19:36:55 +0200

    wip

--- chatgpt_codex_setup_script.sh
+++ chatgpt_codex_setup_script.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-rm -f uv.lock
-uv sync --extra cpu
-uv pip install --group dev
+# rm -f uv.lock
+# uv sync --extra cpu
+# uv pip install --group dev
+uv sync --group dev --no-default-groups
